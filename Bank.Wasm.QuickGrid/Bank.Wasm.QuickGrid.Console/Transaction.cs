12dc271523ab45d8aede4de525b03d01f5695f59
rdw100 <ryandustinwright@hotmail.com>
2023-04-12 08:06:50 -0400

    Added console for oop

---
+++ Bank.Wasm.QuickGrid/Bank.Wasm.QuickGrid.Console/Transaction.cs
@@ -0,0 +1,40 @@
+﻿using System.Formats.Asn1;
+
+namespace Bank.Wasm.QuickGrid.Console;
+
+public class Transaction
+{
+    private static uint _nextId = 12345;
+    private uint _id;
+    public uint Id 
+    { 
+        get => _id; 
+        set 
+        {
+            _nextId++;
+            _id = _nextId;
+        }
+    }    
+
+    public decimal Amount { get; }
+    public DateTime Date { get; }
+    public string Description { get; }
+    public TransactionType Type { get; set; }
+
+    public Transaction(decimal amount, DateTime date, TransactionType txType, string description)
+    {
+        Id = _id;
+        Amount = amount;
+        Date = date;
+        Type = txType;
+        Description = description;
+    }
+}
+
+public enum TransactionType
+{ 
+    Withdraw = 0,
+    Deposit = 1,
+    Check = 2,
+    Debit = 3
+}
